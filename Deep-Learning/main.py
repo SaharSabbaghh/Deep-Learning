746cfb249de93a8f7dae458496c467f4bc143daf
SaharSabbaghh <saharsabbagh3@gmail.com>
2025-02-01 16:42:37 +0200

    Data Visualization

--- Deep-Learning/main.py
+++ Deep-Learning/main.py
@@ -1,1 +1,14 @@
-print('Hello World')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('Daily-gold-data-10years.csv')
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(data['Date'], data['Price'], label='Close Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Gold Price Over Time')
+plt.legend()
+plt.show()
